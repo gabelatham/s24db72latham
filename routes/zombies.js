8aaac51118408bc6c3085005e92729b91226b630
gabelatham <78877534+gabelatham@users.noreply.github.com>
2024-04-15 13:41:27 -0500

    Completed Part 4

--- routes/zombies.js
+++ routes/zombies.js
@@ -2,6 +2,14 @@ var express = require('express');
 const zombie_controller = require('../controllers/zombie_control')
 var router = express.Router();
 
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+        res.redirect("/login");
+    }
+
 /* GET Zombies*/
 router.get('/', zombie_controller.zombie_view_all_page);
 
@@ -12,7 +20,7 @@ router.get('/detail', zombie_controller.zombie_view_one_page)
 router.get('/create', zombie_controller.zombie_create_page)
 
 // GET update zombie page
-router.get('/update', zombie_controller.zombie_update_page)
+router.get('/update', secured, zombie_controller.zombie_update_page)
 
 // GET delete zombie page
 router.get('/delete', zombie_controller.zombie_delete_page)
